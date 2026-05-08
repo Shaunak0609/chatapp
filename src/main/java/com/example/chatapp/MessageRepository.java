3b9f534abbe4596e81b3ac5f5eb6f4d5a6dc34ea
Shaunak Pradhan <shaunpra2016@gmail.com>
2026-05-08 13:21:24 +0400

    Modified code so that when a user delets a room, the messages within the room get deleted as well

--- src/main/java/com/example/chatapp/MessageRepository.java
+++ src/main/java/com/example/chatapp/MessageRepository.java
@@ -3,7 +3,10 @@ package com.example.chatapp;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+
+import jakarta.transaction.Transactional;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
@@ -14,5 +17,12 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
         LIMIT 50
     """, nativeQuery = true)
     List<Message> findLast50MessagesByRoom(String room);
+    
+    @Modifying
+    @Transactional
+    @Query(value = """
+        DELETE FROM messages
+        WHERE room = ?
+    """, nativeQuery = true)
     void deleteByRoom(String room);
 }
